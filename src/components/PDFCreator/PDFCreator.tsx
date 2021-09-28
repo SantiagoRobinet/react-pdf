3037557b91e3ae7b95318ad911b10be7c9c3292a
santiagorobinet <santiagorobinet@gmail.com>
2021-09-28 10:47:38 +0200

    add basic creator files and folders

---
+++ src/components/PDFCreator/PDFCreator.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function PdfCreator(){
+    return <h2>Hi there</h2>
+}
--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import configureStore from './redux/configureStore';
+
+const initialState = {
+  charactersReducer:{}
+}
+
+const store = configureStore(initialState)
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <BrowserRouter> 
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
