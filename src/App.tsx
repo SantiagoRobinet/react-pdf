4bbcc002f185fb6c904baf49724c9a8c8087e5c9
santiagorobinet <santiagorobinet@gmail.com>
2021-09-29 10:32:06 +0200

    add logo and title

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { PDFViewer } from '@react-pdf/renderer';
 
 function App() {
   return (
-    <PDFViewer width={"100%"} height={700}>
+    <PDFViewer width={"100%"} height={window.innerHeight-20} >
       <PdfCreator />
      </PDFViewer>
 );
--- src/components/PDFCreator/PDFCreator.tsx
+++ src/components/PDFCreator/PDFCreator.tsx
@@ -1,27 +1,68 @@
 import React from "react";
 
-import { Page, Text, View, Document, StyleSheet } from '@react-pdf/renderer';
+import { Page, Text, View, Document, StyleSheet, Image} from '@react-pdf/renderer';
 
 // Create styles
 const styles = StyleSheet.create({
   page: {
     flexDirection: 'row',
     backgroundColor: '#E4E4E4',
+    padding:0
   },
   section: {
-    margin: 10,
-    padding: 10,
-    flexGrow: 1
-  }
+    flexGrow: 1,
+    position: "relative",
+    backgroundColor:"white",
+  },
+  innerSection: {
+    flexGrow: 1,
+    padding:73
+  },
+  image:{
+      width:225,
+      position:"absolute",
+      right:215,
+      bottom:133
+  },
+  cyanCloud:{
+      height:"100%",
+      width:208,
+      position:"absolute",
+      right:0
+  },
+  grayCloud:{
+    height:"100%",
+    width:208,
+    position:"absolute",
+    right:150,
+  },
+  logo:{
+    width:100,
+},
+  title:{
+    fontSize:30,
+    paddingTop:24,
+    paddingBottom:16,
+    borderBottom:2,
+    width:410
+},
+
 });
 
 // Create Document Component
 export default function MyDocument() {
+    const algo = "Pepelandia"
     return(
     <Document>
     <Page size={{width:1014, height: 717}} style={styles.page}>
       <View style={styles.section}>
-        <Text>Section #1</Text>
+        <Image style={styles.grayCloud} src="/grayCloud.png" />
+        <Image style={styles.cyanCloud} src="/cyanCloud.png" />
+        <Image style={styles.image} src="/ESG.png" />
+        <View style={styles.innerSection}>
+            <Image style={styles.logo} src="/pulse_logo_dark.png" />
+            <Text style={styles.title}>Certificate of Completition</Text>
+        </View>
       </View>
     </Page>
     </Document>
